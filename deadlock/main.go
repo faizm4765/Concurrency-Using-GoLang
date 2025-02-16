21c3a4b874d52ef15ba27122aee60b493868afda
faizm4765 <faizm4765@gmail.com>
2025-02-16 10:49:12 +0530

    code gave deadlock error

--- deadlock/main.go
+++ deadlock/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 var wg sync.WaitGroup
+var mu [6]sync.Mutex
 
 func main() {
 	// 6 threads trying to acquire 2 resources in a circular way. Simulate deadlock.
@@ -15,7 +17,24 @@ func main() {
 		go func(id int) {
 			defer wg.Done()
 			fmt.Println(id)
+			resource1 := id
+			resource2 := (id + 1) % 6
+
+			fmt.Printf("Thread %d trying to acquire resource %d\n", id, resource1)
+			mu[resource1].Lock()
+			fmt.Printf("Thread %d acquired resource %d\n", id, resource1)
+
+			// Simulate some work
+			time.Sleep(1 * time.Second)
+
+			fmt.Printf("Thread %d trying to acquire resource %d\n", id, resource2)
+			mu[resource2].Lock()
+			fmt.Printf("Thread %d acquired resource %d\n", id, resource2)
+
+			mu[resource1].Unlock()
+			mu[resource2].Unlock()
 		}(i)
 	}
+
 	wg.Wait()
 }
