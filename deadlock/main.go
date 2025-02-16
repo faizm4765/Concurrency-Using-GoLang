7c305c2ff6659c84af39997f8b6ad15e58a1c360
faizm4765 <faizm4765@gmail.com>
2025-02-16 10:42:27 +0530

    added basic go routine structure

--- deadlock/main.go
+++ deadlock/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	// 6 threads trying to acquire 2 resources in a circular way. Simulate deadlock.
+
+	for i := 0; i < 6; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			fmt.Println(id)
+		}(i)
+	}
+	wg.Wait()
+}
