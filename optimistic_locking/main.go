f3110a80fec21e34adf7f8884821760ca3801666
faizm4765 <faizm4765@gmail.com>
2025-02-22 15:20:49 +0530

    implmented optimistic locking

--- optimistic_locking/main.go
+++ optimistic_locking/main.go
@@ -1,22 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
 
 type Counter struct {
-	value   int
-	version int
+	value   int64
+	version int64
 }
 
-func (c *Counter) Update(newValue int) {
-	c.value = newValue
-	c.version++
-	fmt.Println("Updated value of counter:", c.value)
+var wg sync.WaitGroup
+
+func (c *Counter) Update(newValue int64) {
+	defer wg.Done()
+	curVal := atomic.LoadInt64(&c.value)
+
+	if atomic.CompareAndSwapInt64(&c.value, curVal, newValue) {
+		atomic.AddInt64(&c.version, 1)
+		atomic.StoreInt64(&c.value, newValue)
+		fmt.Println("Updated value of counter:", c.value)
+		return
+	}
+
+	fmt.Println("Failed to update counter!")
+	return
 }
 
 func main() {
 	counter := Counter{value: 0, version: 0}
-	counter.Update(10)
-	counter.Update(20)
+	wg.Add(2)
+	go counter.Update(10)
+	go counter.Update(20)
+	// var i int64
+	// for i = 1; i < 10; i++ {
+	// 	wg.Add(1)
+	// 	go counter.Update(i)
+	// }
 
+	wg.Wait()
 	fmt.Println("Final value of counter:", counter.value)
 }
